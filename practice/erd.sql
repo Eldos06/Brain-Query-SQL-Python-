fde799937a95cfa27faeab6faf8eac957b74a730
YeldoS2 <eldossuleimen2006@gmail.com>
2025-11-13 22:04:30 +0500

    Yeldos created all tables and half of ALTER TABLES

--- practice/erd.sql
+++ practice/erd.sql
@@ -1,125 +1,145 @@
-CREATE TABLE Note_media(
-    NoteMediaID INTEGER PRIMARY KEY
-    UsageContext VARCHAR(1000)
-    FOREIGN KEY (NoteID) REFERENCES Note(NoteID)
-    FOREIGN KEY (MediaID) REFERENCES Media(MediaID)
+--CREATE DATABASE FlashCards
+--GO;
+
+--USE FlashCards
+--GO;
+
+CREATE TABLE tblCARD (
+CardID INT IDENTITY(1, 1) PRIMARY KEY,
+CardName VARCHAR(50) NOT NULL,
+CardDescr VARCHAR(500) NULL
 )
 
-CREATE TABLE Note(
-    NoteID INTEGER PRIMARY KEY
-    Guid VARCHAR(100)
-    CreatedAt TIMESTAMP
-    UpdateAt TIMESTAMP
-    FOREIGN KEY (ModelID) REFERENCES Note_model(ModelID)
-    FOREIGN KEY (OwnerUserID) REFERENCES User(UserID)
+CREATE TABLE tblDECK (
+DeckID INT IDENTITY(1, 1) PRIMARY KEY,
+DeckName VARCHAR(100),
+DeckDescr VARCHAR(500),
 )
 
-CREATE TABLE Note_model(
-    ModelID INTEGER PRIMARY KEY
-    ModelName VARCHAR(100)
-    Description VARCHAR(10000)   
+CREATE TABLE tblSUBJECT (
+SubjectID INT IDENTITY(1, 1) PRIMARY KEY,
+SubjectName VARCHAR(50),
+SubjectDescr VARCHAR(500)
 )
 
-CREATE TABLE Note_field(
-    FieldID INTEGER PRIMARY KEY
-    Description TEXT
-    FieldName VARCHAR(100)
-    Ordinal INTEGER
+CREATE TABLE tblOWNER (
+OwnerID INT IDENTITY(1, 1) PRIMARY KEY,
+OwnerFname VARCHAR(50),
+OwnerLname VARCHAR(50),
+OwnerDescr VARCHAR(500),
 )
 
-CREATE TABLE Model_field(
-    ModelFieldID INTEGER PRIMARY KEY
-    FieldName VARCHAR(100)
-    Ordinal INTEGER
-    FOREIGN KEY (ModelID) REFERENCES Note_model(ModelID)
+CREATE TABLE tblDECK_TAG (
+DeckTagID INT IDENTITY(1, 1) PRIMARY KEY,
 )
 
-CREATE TABLE Note_field_value(
-    NoteFieldID INTEGER PRIMARY KEY
-    FieldValue TEXT
-    FOREIGN KEY (NoteID) REFERENCES Note(NoteID)
-    FOREIGN KEY (ModelFieldID) REFERENCES Model_field(ModelFieldID)
+CREATE TABLE tblTAG (
+TagID INT IDENTITY(1, 1) PRIMARY KEY,
+TagName VARCHAR(50),
+TagDescr VARCHAR(500),
 )
 
-CREATE TABLE Model_template(
-    TemplateID INTEGER PRIMARY KEY
-    TemplateName VARCHAR(100)
-    FrontTemplate TEXT
-    BackTemplate TEXT
-    CSS TEXT
-    FOREIGN KEY (ModelID) REFERENCES Note_model(ModelID)   
+CREATE TABLE tblSIDE (
+SideID INT IDENTITY(1, 1) PRIMARY KEY,
+SideName VARCHAR(5),
+SideDescr VARCHAR(500),
 )
 
-CREATE TABLE Note_tag(
-    NoteTagID INTEGER PRIMARY KEY
-    FOREIGN KEY (NoteID) REFERENCES Note(NoteID)
-    FOREIGN KEY (TagID) REFERENCES Tag(TagID)
+CREATE TABLE tblASSIGNMENT_CARD (
+AssignmentCardID INT IDENTITY(1, 1) PRIMARY KEY,
+BeginDateTime TIMESTAMP,  -- TIMESTAMP - format: YYYY-MM-DD HH:MM:SS
+EndDateTime DATETIME,    -- TIMESTAMP - format: YYYY-MM-DD HH:MM:SS
 )
 
-CREATE TABLE Tag(
-    TagID INTEGER PRIMARY KEY
-    TagName VARCHAR(100)
-    Description TEXT
+CREATE TABLE tblASSIGNMENT (
+AssignmentID INT IDENTITY(1, 1) PRIMARY KEY,
 )
 
-CREATE TABLE User (
-        UserID INTEGER PRIMARY KEY
-        email VARCHAR(100) UNIQUE NOT NULL
-        password_hash VARCHAR(255) NOT NULL
-        display_name VARCHAR(100)
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE tblSTUDENT (
+StudentFname VARCHAR(50) NOT NULL,
+StudentLname VARCHAR(50) NOT NULL,
+BirthDate TIMESTAMP NULL,
+email VARCHAR(100),
+)
+
+CREATE TABLE tblMEDIA_CARD (
+MediaCardID INT IDENTITY(1, 1) PRIMARY KEY,
+--Img LONGBLOB NULL, -- Image, Instance of Size(MB) We had Img
+Words TEXT NULL
+)
+
+CREATE TABLE tblMEDIA (
+MediaID INT IDENTITY(1, 1) PRIMARY KEY,
+MediaName VARCHAR(30) NOT NULL,
+MediaDescr VARCHAR(500) NULL,
+)
+
+CREATE TABLE tblNOTE (
+NoteID INT IDENTITY(1, 1) PRIMARY KEY,
+NoteDate TIMESTAMP,
+)
+
+CREATE TABLE tblREVIEW (
+ReviewID INT IDENTITY(1, 1) PRIMARY KEY,
+ReviewDate TIMESTAMP,
+)
+
+CREATE TABLE tblRATING (
+RatingID INT IDENTITY(1, 1) PRIMARY KEY,
+RatingName VARCHAR(50) NULL,
+)
+
+CREATE TABLE tblINTERVAL (
+IntervalID INT IDENTITY(1, 1) PRIMARY KEY,
+DueDate TIMESTAMP NOT NULL,  -- After this time u should repeat that card
+--- I DON'T NEED IntervalName  AND IntervalDescr
 )
 
 
-CREATE TABLE Media (
-      MediaID INTEGER  PRIMARY KEY
-      OwnerUserID REFERENCES users(UserID) ON DELETE CASCADE
-      file_name VARCHAR(255) NOT NULL
-      mime_type VARCHAR(100)
-      size_bytes BIGINT
-      sha256 CHAR(64)
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+-------------------
+-- ALTER TABLES
+--------------------
+ALTER TABLE tblDECK
+ADD SubjectID INT NULL,
+	OwnerID   INT NULL;
+	GO
+ALTER TABLE tblDECK
+ADD CONSTRAINT FK_DECK_SUBJECT
+	FOREIGN KEY (SubjectID)
+	REFERENCES tblSUBJECT(SubjectID),
+    CONSTRAINT FK_DECK_OWNER
+	FOREIGN KEY (OwnerID)
+	REFERENCES tblOWNER(OwnerID);
 
 
-CREATE TABLE Desk(
-      DeskID INTEGER  PRIMARY KEY
-      OwnerUserID REFERENCES Users(UserID) ON DELETE CASCADE
-      DeskName VARCHAR(100) NOT NULL
-      Description TEXT
-      CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      isPublic BOOLEAN DEFAULT FALSE
- )
+ALTER TABLE tblDECT_TAG
+ADD CONSTRAINT FK_DectTag_DECK
+	FOREIGN KEY (DeckID)
+	REFERENCES tblDECK(DeckID),
+	CONSTRAINT FK_DeckTag_Tag
+	FOREIGN KEY (TagID)
+	REFERENCES tblTAG(TagID)
 
-CREATE TABLE Card(
-      CardID INTEGER PRIMARY KEY
-      NoteID  REFERENCES Note(NoteID) ON DELETE CASCADE
-      TemplateID  REFERENCES Model_template(TemplateID)
-      DeskID  REFERENCES desk(DeskID)
-      DueAt TIMESTAMP
-      EasyFactor FLOAT
-      Intervals INT
-      Reps INT
-      Lapses INT DEFAULT 0,
-      State VARCHAR(50)
-)
 
-CREATE TABLE Deck_tag(
-      DeckTagID INTEGER PRIMARY KEY
-      DeskID  REFERENCES Desk(DeskID) ON DELETE CASCADE
-      TagId  REFERENCES Tag(TagID) ON DELETE CASCADE
-)
 
-CREATE TABLE Review_log(
-      ReviewID INTEGER PRIMARY KEY
-      CardID  REFERENCES Card(CardID) ON DELETE CASCADE
-      ReviewedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      Rating INT CHECK (Rating BETWEEN 1 AND 5)
-      TimeTakenMs INT
-      OldInterval INT
-      NewInterval INT
-      OldEase FLOAT
-      NewEase FLOAT
-)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
